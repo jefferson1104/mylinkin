678f316ef48b3bceff35dc2825845c0620803501
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-03-30 03:43:14 -0300

    fix: update clicks

--- packages/back-end/src/server.ts
+++ packages/back-end/src/server.ts
@@ -42,24 +42,28 @@ app.register(fastifySwaggerUi, {
 app.get('/:code', async (request, reply) => {
     const getLinkSchema = z.object({
         code: z.string().min(3),
-    })
+    });
 
-    const { code } = getLinkSchema.parse(request.params)
+    const { code } = getLinkSchema.parse(request.params);
 
     try {
         const result = await sql`
-            SELECT id, original_url FROM short_links WHERE short_links.code = ${code}
-        `
+            SELECT id, original_url, clicks FROM short_links WHERE short_links.code = ${code.toUpperCase()}
+        `;
 
         if (result.length === 0) {
             return reply.status(400).send({ message: 'Link not found.' })
-        }
+        };
 
-        const link = result[0]
+        const link = result[0];
+        const linkId = link.id;
+        const updatedLinkClicks = link.clicks + 1;
 
-        await redis.zIncrBy('metrics', 1, String(link.id))
+        await sql`UPDATE short_links SET clicks = ${updatedLinkClicks} WHERE id = ${linkId}`
 
-        return reply.redirect(301, link.original_url)
+        await redis.zIncrBy('metrics', 1, String(link.id));
+
+        return reply.redirect(301, link.original_url);
     } catch (error) {
         console.error(error)
         return reply.status(500).send({ message: 'Internal server error.' })
