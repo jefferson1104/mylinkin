ccd765ca8e87ba888b0ef483aa242f0e1c67379e
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-03-31 18:12:24 -0300

    fix: public assets

--- packages/front-end/src/app/components/Navbar/Navbar.tsx
+++ packages/front-end/src/app/components/Navbar/Navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
       <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
         <Link href="/" className="flex items-center font-display text-2xl">
           <Image
-            src="/logo.svg"
+            src="/images/logo.svg"
             alt="Short Links"
             width={40}
             height={40}
