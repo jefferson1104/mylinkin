93ead3fe61afe7d50e6eb94691a1adc777afd530
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-04-17 10:21:12 -0300

    front-end README.md

---
+++ packages/front-end/README.md
@@ -0,0 +1,38 @@
+<div align="center" style="margin-bottom: 20px;">
+  <div>
+    <img src="./packages/front-end/public/images/banner.png" />
+  </div>
+
+  <div align="center">
+    <img alt="technology" src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB">
+    <img alt="technology" src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white">
+    <img alt="technology" src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white">
+    <img alt="technology" src="https://img.shields.io/badge/radix%20ui-161618.svg?style=for-the-badge&logo=radix-ui&logoColor=white">
+    <img alt="technology" src="https://img.shields.io/badge/Framer-black?style=for-the-badge&logo=framer&logoColor=blue">
+  </div>
+</div>
+
+## :memo: About project
+
+This is the front-end of the MyLinkIn, developed with Next.js 14, TypeScript, and other technologies. This project has interesting features like, internationalization, light/dark mode, good practices for accessibility and SEO, primitive components with Radix Ui, forms and forms validations, and others.
+
+## :rocket: Technologies
+
+- [Next.js](https://nextjs.org/docs)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Radix-UI](https://www.radix-ui.com/)
+- [Lucid React](https://lucide.dev/icons/)
+- [Framer Motion](https://www.framer.com/motion/)
+- [Next Intl](https://next-intl-docs.vercel.app/)
+- [Vaul](https://vaul.emilkowal.ski/)
+- [Joi](https://joi.dev/)
+
+## :cyclone: Run this project
+
+```bash
+# Install dependencies
+$ npm install
+
+# Run project on localhost:3000
+$ npm run dev
+```
--- packages/front-end/src/app/[locale]/layout.tsx
+++ packages/front-end/src/app/[locale]/layout.tsx
@@ -22,56 +22,57 @@ export const metadata: Metadata = {
   icons: ["/images/logo-light.svg", "/images/logo-light.svg"],
   metadataBase: new URL("https://mylinkin.com"),
   description:
-      "The best link shortener, at MyLinIn you can shorten your links, customize and check how many clicks you got.",
+    "The best link shortener, at MyLinIn you can shorten your links, customize and check how many clicks you got.",
   alternates: {
-      canonical: "https://mylinkin.com"
+    canonical: "https://mylinkin.com"
   },
   appleWebApp: {
-      capable: true,
-      title: "MyLinkIn"
+    capable: true,
+    title: "MyLinkIn"
   },
   authors: [
-      {
-          name: "SoaresDev",
-          url: "https://soaresdev.com"
-      },
-      {
-          name: "Jefferson Soares",
-          url: "https://links.soaresdev.com"
-      }
+    {
+      name: "SoaresDev",
+      url: "https://soaresdev.com"
+    },
+    {
+      name: "Jefferson Soares",
+      url: "https://links.soaresdev.com"
+    }
   ],
   keywords: [
-      "link shortener",
-      "url shortner",
-      "shortened url",
-      "shortened links",
-      "short link",
-      "short url",
-      "short links",
-      "links analytics",
-      "encurtador de links",
-      "encurtar links",
-      "encurtador de url",
-      "encurtar url",
-      "links curtos",
-      "url encurtada",
-      "links encurtados",
-      "mylinkin",
-      "my link in"
+    "link shortener",
+    "url shortner",
+    "shortened url",
+    "shortened links",
+    "short link",
+    "short url",
+    "short links",
+    "links analytics",
+    "encurtador de links",
+    "encurtar links",
+    "encurtador de url",
+    "encurtar url",
+    "links curtos",
+    "url encurtada",
+    "links encurtados",
+    "mylinkin",
+    "my link in"
   ],
   openGraph: {
-      type: "website",
-      url: "https://milinkin.com",
-      title: "MyLinkIn - Link Shortener",
-      description: "The best link shortener, at MyLinIn you can shorten your links, customize and check how many clicks you got.",
-      siteName: "MyLinkIn - Link Shortener",
-      images: [
-          {
-            url: "/images/banner.png"
-          }
-      ]
+    type: "website",
+    url: "https://milinkin.com",
+    title: "MyLinkIn - Link Shortener",
+    description: "The best link shortener, at MyLinIn you can shorten your links, customize and check how many clicks you got.",
+    siteName: "MyLinkIn - Link Shortener",
+    images: [
+      {
+        url: "/images/banner.png"
+      }
+    ]
   }
 };
+
 export default function RootLayout({
   children,
   params: { locale }
